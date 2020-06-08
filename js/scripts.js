e8f440503beb2e4edcb7d855dde9fde323834bd1
Taylor Somers <tsomers@yandex.com>
2020-06-08 15:01:34 -0400

    Create unordered list in which to display sampled results of user inputed favorite things

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,22 @@
 $(document).ready(function() {
   $("button").click(function(event) {
-    const favBand = $("input#band").val();
-    const favSeason = $("input#season").val();
-    const favArchitecture = $("input#architecture").val();
-    const favAnimal = $("input#animal").val();
-    const favMovie = $("input#movie").val();
-    const favArray = [favBand,favSeason,favArchitecture,favAnimal,favMovie];
+    let favBand = $("input#band").val();
+    let favSeason = $("input#season").val();
+    let favArchitecture = $("input#architecture").val();
+    let favAnimal = $("input#animal").val();
+    let favMovie = $("input#movie").val();
+    let favArray = [favBand,favSeason,favArchitecture,favAnimal,favMovie];
     
     let resultArray = [];
     resultArray.push(favArray[1],favArray[0],favArray[2]);
 
-    $("#results").text(resultArray);
+    let result1 = resultArray[0];
+    let result2 = resultArray[1];
+    let result3 = resultArray[2];
+
+    $("#result1").text(result1);
+    $("#result2").text(result2);
+    $("#result3").text(result3);
 
   (event).preventDefault();
   });
